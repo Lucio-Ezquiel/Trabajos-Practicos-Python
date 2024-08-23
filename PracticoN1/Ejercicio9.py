da12a105f6a19c5987560321bef22184ac9416c6
Lucio Ezequiel Arena <arenalucio.mdz@gmail.com>
2024-08-23 11:25:38 -0300

    Ejercicios 1.5.9

---
+++ PracticoN1/Ejercicio9.py
@@ -0,0 +1,10 @@
+#Muestro por pantalla el 1.
+print("1")
+
+#Bucle for que recorre los números del 1 al 100, y solo muestra por pantalla los que son divisibles por 2 y 3.
+for i in range  (1,100):
+ if i % 2 == 0 or i % 3 == 0:
+    print(i)
+
+#Muestro por pantalla el 100.
+print("100")
