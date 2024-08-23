ce4b3be8907b77a3e99b51ff249ac3491666506c
moonstfw <nfacundoluna@gmail.com>
2024-08-23 20:06:20 -0300

    ejercio 11!

--- PracticoN1/Ejercicio3.py
+++ PracticoN1/Ejercicio3.py
@@ -1,1 +1,11 @@
-print("Chau")
+""" Declare dos variables numéricas (con el valor que desees), e indica cual es mayor de los dos. Si son iguales indicarlo también """
+
+numero1 = 4
+numero2 = 5
+
+if numero1 > numero2:
+    print(numero1,"es mayor que", numero2)
+elif numero1 < numero2:
+    print(numero2, "es mayor que ", numero1 )
+else:
+    print("Son iguales")
