21c29623e324065960ee8a092f415d55a5810282
Mariano <mariagu04@outlook.com.ar>
2024-08-28 19:04:43 -0300

    Update Ejercicio4.py

--- PracticoN1/Ejercicio4.py
+++ PracticoN1/Ejercicio4.py
@@ -1,6 +1,8 @@
-numero1 = 4
-numero2 = 5
+numero1 = 6
+numero2 = 7
 if numero1 > numero2:
-    print(numero1,"es mayor que", numero2)
+    print(f"{numero1} es mayor que {numero2}")
+elif numero2 > numero1:
+    print(f"{numero2} es mayor que {numero1}")
 else:
-    print(numero2, "es mayor que ", numero1 )
+    print(f"{numero1} y {numero2} son iguales")
