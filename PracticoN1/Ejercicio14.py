ab84a96c6923afcbb1468d4e09963e985f6cb552
Lucio Ezequiel Arena <arenalucio.mdz@gmail.com>
2024-09-06 10:20:21 -0300

    Ejercicio 14 correjido

--- PracticoN1/Ejercicio14.py
+++ PracticoN1/Ejercicio14.py
@@ -1,16 +1,15 @@
 import random
 
+numIngresado = 0
 contador = 1
 numAleatorio = random.randint(1, 100)
-numIngresado = int(input("Introduce un numero entre 1 y 100: "))
 print(numAleatorio)
 while (numIngresado != numAleatorio):
+    numIngresado = int(input("Introduce un numero entre 1 y 100: "))
     if (numIngresado > numAleatorio):
         print("El numero es menor")
-        numIngresado = int(input("Introduce un numero entre 1 y 100: "))
     else:
         print("El numero es mayor")
-        numIngresado = int(input("Introduce un numero entre 1 y 100: "))
     contador += 1
 
 print("Encontraste el numero en", contador, "intentos")
